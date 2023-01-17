6e25708a7473b0d8cb8ba6f0c04f9bb932c41162
Kyu Lee <kwooler@gmail.com>
2023-01-17 11:51:39 -0800

    A32

--- main_test.py
+++ main_test.py
@@ -7,20 +7,18 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    # datastr = '10\n2\n2\n'
-    # sys.stdin = io.StringIO(datastr)
+    datastr = '10\n2\n1\n'
+    sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
-    rvalues = list(map(int, lines[0].split()))
-    print(rvalues)
-    minval = min(rvalues)
     print(lines)
 
-    regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    res = re.search(regex_string, lines[1])
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    res = re.search(r'[\w,\W]*1[\w,\W]*2[\w,\W]*10[\w,\W]*', lines[0])
     assert res != None
     print(res.group())
 
@@ -28,19 +26,36 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    # datastr = '10\n2\n2\n'
-    # sys.stdin = io.StringIO(datastr)
+    datastr = '2\n3\n1\n'
+    sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
-    rvalues = list(map(int, lines[0].split()))
-    print(rvalues)
-    minval = min(rvalues)
     print(lines)
 
-    regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    res = re.search(regex_string, lines[1])
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    res = re.search(r'[\w,\W]*1[\w,\W]*2[\w,\W]*3[\w,\W]*', lines[0])
+    assert res != None
+    print(res.group())
+
+
+def test_main_3():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '2\n1\n3\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    res = re.search(r'[\w,\W]*1[\w,\W]*2[\w,\W]*3[\w,\W]*', lines[0])
     assert res != None
     print(res.group())
