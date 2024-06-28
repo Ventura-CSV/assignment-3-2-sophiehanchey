39ec9d6b8bb933054441556f97d506f350bfd01d
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-27 20:42:47 -0700

    error in ordering, initial code

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    #Find Minimum
+    minval = num1
+    if(num2<num1):
+        minval = num2
+    elif(num3<num2):
+        minval = num3
+    
+    #Find Maximum
+    maxval = num1
+    if(num2>num1):
+        maxval = num2
+    elif(num3>num2):
+        maxval = num3
+    
+    #Set Median
+    if(num1!=minval & num1!=maxval):
+        median = num1
+    elif(num2!=minval & num2!=maxval):
+        median = num2
+    elif(num3!=minval & num3!=maxval):
+        median = num3
+    else:
+        median = num1
 
     print(minval, median, maxval)
     ########################################
